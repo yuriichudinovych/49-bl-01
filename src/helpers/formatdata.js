e987c87e037e790aa969b110308e6427788603fc
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-09-18 13:05:35 +0300

    add task-4

--- src/helpers/formatdata.js
+++ src/helpers/formatdata.js
@@ -1,5 +1,10 @@
 import { formatDistanceToNow } from 'date-fns';
+import { format } from 'date-fns'
 
 export const formatData = data => {
   return formatDistanceToNow(new Date(data), { addSuffix: true });
 };
+
+export const formatHistory = data => {
+  return format(new Date(data), 'MM/dd/yyyy', { addSuffix: true })
+}
